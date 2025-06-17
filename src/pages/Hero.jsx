84122a594e771e0ee97f8b816840d9aab2e771c0
brandaorichard <rih.brandao@gmail.com>
2025-06-17 11:02:24 -0300

    feat: remove unused profile pictures and update Hero component image import

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profpic from "../assets/images/profpicc.jpg";
+import profpic from "../assets/images/profile.jpg";
 
 export const Hero = () => {
   return (
